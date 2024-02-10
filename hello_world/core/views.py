e387d7158d551fc782ee47c23f08977ddc3fe1a1
Sohaib radi <sohaib.radi@gmail.com>
2024-02-10 12:50:32 +0000

    modified:   hello_world/core/views.py 	modified:   hello_world/templates/index.html

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
+    x = 5;
+    y = calculateSome(80,90)
     context = {
+        'x':y,
         "title": "Django example",
     }
     return render(request, "index.html", context)
+
+
+def calculateSome(x,y):
+    return x+y;
